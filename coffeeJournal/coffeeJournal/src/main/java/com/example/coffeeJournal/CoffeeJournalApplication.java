3873c5b84c4e53d63f6f96d93cae348e2716b416
stephveile <veile.steph@gmail.com>
2025-10-23 20:03:12 -0500

    Initial commit

---
+++ coffeeJournal/coffeeJournal/src/main/java/com/example/coffeeJournal/CoffeeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeeJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeJournalApplication.class, args);
+	}
+
+}
---
+++ coffeeJournal/coffeeJournal/src/test/java/com/example/coffeeJournal/CoffeeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeJournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
